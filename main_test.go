e2fcc5b8aeeda830dc6b2b9176142f2edadc2670
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-08-11 19:22:32 +0300

    Drop gocheck

--- main_test.go
+++ main_test.go
@@ -1,118 +1,122 @@
 package main
 
 import (
+	"testing"
 	"fmt"
 	"io/ioutil"
-	. "launchpad.net/gocheck"
-	"math/rand"
 	"path/filepath"
-	"testing"
 	"time"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
+const equals = true
 
-type TestSuite struct{}
-
-var _ = Suite(&TestSuite{})
-
-func (s *TestSuite) SetUpSuite(c *C) {
-	*environment = "test"
-	rand.Seed(time.Now().UTC().UnixNano())
-	redisPool = getRedisPool(*redisHost)
+func assert(t *testing.T, a interface{}, mustEqual bool, b interface{}) {
+	if mustEqual {
+		if a != b {
+			t.Fatalf("%v did not equal %v", a, b)
+		}
+	} else {
+		if a == b {
+			t.Fatalf("%v did equal %v", a, b)
+		}
+	}
 }
 
-func (s *TestSuite) TearDownSuite(c *C) {
-	redisPool.Close()
-	redisPool = nil
-}
-
-func (s *TestSuite) TestParseEntry(c *C) {
+func TestParseEntry(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-12-26 12:46:5;75942;60;3158;5632;1584;144>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(fmt.Sprintf("%d", entry.SensorID), Equals, "75942")
-	c.Assert(entry.NextDataSession, Equals, "60")
-	c.Assert(entry.BatteryVoltage, Equals, "3158")
-	c.Assert(entry.Sensor1, Equals, "5632")
-	c.Assert(entry.Sensor2, Equals, "1584")
-	c.Assert(entry.RadioQuality, Equals, "144")
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, fmt.Sprintf("%d", entry.SensorID), equals, "75942")
+	assert(t, entry.NextDataSession, equals, "60")
+	assert(t, entry.BatteryVoltage, equals, "3158")
+	assert(t, entry.Sensor1, equals, "5632")
+	assert(t, entry.Sensor2, equals, "1584")
+	assert(t, entry.RadioQuality, equals, "144")
 }
 
-func (s *TestSuite) TestParseDateTime(c *C) {
+func TestParseDateTime(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-12-26 12:46:5;75942;60;3158;5632;1584;144>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(entry.Datetime.Year(), Equals, 2012)
-	c.Assert(entry.Datetime.Month(), Equals, time.December)
-	c.Assert(entry.Datetime.Day(), Equals, 26)
-	c.Assert(entry.Datetime.Hour(), Equals, 12)
-	c.Assert(entry.Datetime.Minute(), Equals, 46)
-	c.Assert(entry.Datetime.Second(), Equals, 5)
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, entry.Datetime.Year(), equals, 2012)
+	assert(t, entry.Datetime.Month(), equals, time.December)
+	assert(t, entry.Datetime.Day(), equals, 26)
+	assert(t, entry.Datetime.Hour(), equals, 12)
+	assert(t, entry.Datetime.Minute(), equals, 46)
+	assert(t, entry.Datetime.Second(), equals, 5)
 	name, _ := entry.Datetime.Zone()
-	c.Assert(name, Equals, "UTC")
+	assert(t, name, equals, "UTC")
 }
 
-func (s *TestSuite) TestParseDateTimeMonthAndDayNotPadded(c *C) {
+func TestParseDateTimeMonthAndDayNotPadded(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-2-5 12:46:5;75942;60;3158;5632;1584;144>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(entry.Datetime.Year(), Equals, 2012)
-	c.Assert(entry.Datetime.Month(), Equals, time.February)
-	c.Assert(entry.Datetime.Day(), Equals, 5)
-	c.Assert(entry.Datetime.Hour(), Equals, 12)
-	c.Assert(entry.Datetime.Minute(), Equals, 46)
-	c.Assert(entry.Datetime.Second(), Equals, 5)
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, entry.Datetime.Year(), equals, 2012)
+	assert(t, entry.Datetime.Month(), equals, time.February)
+	assert(t, entry.Datetime.Day(), equals, 5)
+	assert(t, entry.Datetime.Hour(), equals, 12)
+	assert(t, entry.Datetime.Minute(), equals, 46)
+	assert(t, entry.Datetime.Second(), equals, 5)
 	name, _ := entry.Datetime.Zone()
-	c.Assert(name, Equals, "UTC")
+	assert(t, name, equals, "UTC")
 }
 
-func (s *TestSuite) TestParseDateTimeWithPaddedSeconds(c *C) {
+func TestParseDateTimeWithPaddedSeconds(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-12-26 12:46:05;75942;60;3158;5632;1584;144>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(entry.Datetime.Second(), Equals, 5)
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, entry.Datetime.Second(), equals, 5)
 }
 
-func (s *TestSuite) TestParseDateTimeSeconds(c *C) {
+func TestParseDateTimeSeconds(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-12-26 12:46:35;75942;60;3158;5632;1584;144>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(entry.Datetime.Second(), Equals, 35)
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, entry.Datetime.Second(), equals, 35)
 }
 
-func (s *TestSuite) TestParseDateTimeMinutes(c *C) {
+func TestParseDateTimeMinutes(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	entry, err := NewEntry("<2012-12-26 13:2:36;75942;10;3202;6784;1580;150>")
-	c.Assert(err, IsNil)
-	c.Assert(entry, Not(IsNil))
-	c.Assert(entry.Datetime.Minute(), Equals, 2)
+	assert(t, err, equals, nil)
+	assert(t, entry, !equals, nil)
+	assert(t, entry.Datetime.Minute(), equals, 2)
 }
 
-func (s *TestSuite) TestProcessEntries(c *C) {
+func TestProcessEntries(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	b, err := ioutil.ReadFile(filepath.Join("test", "testfile.txt"))
-	c.Assert(err, IsNil)
+	assert(t, err, equals, nil)
 	count, err := ProcessEntries(string(b))
-	c.Assert(err, IsNil)
-	c.Assert(count, Equals, 107)
+	assert(t, err, equals, nil)
+	assert(t, count, equals, 107)
 }
 
-func (s *TestSuite) TestProcessExample(c *C) {
+func TestProcessExample(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	b, err := ioutil.ReadFile(filepath.Join("test", "example.txt"))
-	c.Assert(err, IsNil)
+	assert(t, err, equals, nil)
 	count, err := ProcessEntries(string(b))
-	c.Assert(err, IsNil)
-	c.Assert(count, Equals, 5)
+	assert(t, err, equals, nil)
+	assert(t, count, equals, 5)
 }
 
-func (s *TestSuite) TestProcessSingleLineExample(c *C) {
+func TestProcessSingleLineExample(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	count, err := ProcessEntries("<2013-4-7 10:24:39;426842;60;3135;6656;2312;126>")
-	c.Assert(err, IsNil)
-	c.Assert(count, Equals, 1)
+	assert(t, err, equals, nil)
+	assert(t, count, equals, 1)
 }
 
-func (s *TestSuite) TestProcessSingleLineStartingWithR(c *C) {
+func TestProcessSingleLineStartingWithR(t *testing.T) {
+	redisPool = getRedisPool(*redisHost); defer redisPool.Close()
 	count, err := ProcessEntries("\r<2013-4-7 10:24:39;426842;60;3135;6656;2312;126>")
-	c.Assert(err, IsNil)
-	c.Assert(count, Equals, 1)
+	assert(t, err, equals, nil)
+	assert(t, count, equals, 1)
 }
